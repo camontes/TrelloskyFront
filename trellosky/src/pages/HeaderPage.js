d1e9c2fea0853e780bab00ecede8ac16eaf422db
camontes <pope624@hotmail.com>
2023-01-09 16:34:04 -0500

    Create Arquitecture

---
+++ trellosky/src/pages/HeaderPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+const HeaderPage = () => {
+
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
+
+
+export default HeaderPage;
